1cc01c4df6c6c00d22604a81ebf492a673d97331
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-05-24 06:28:00 +0000

    update

---
+++ yulm-styled/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yulm-styled',
+    version='1.0.0',
+    author='LopeKinz',
+    author_email='pinkyhax@gmail.com',
+    description='A package for colorizing and styling text',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/lopekinz/yulm-styled',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
