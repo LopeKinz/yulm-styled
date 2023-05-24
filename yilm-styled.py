62f2420c4f5fe841ff0e8aadf608117764e35fb6
LopeKinz <48872005+LopeKinz@users.noreply.github.com>
2023-05-24 06:17:40 +0000

    v.0.1

--- yilm-styled.py
+++ yilm-styled.py
@@ -1,5 +1,5 @@
 import sys
-
+import time
 
 class Color:
     """
@@ -68,9 +68,53 @@ def rainbow_text(text):
     rainbow_text += Color.RESET
     return rainbow_text
 
+class Colorr:
+    """
+    Class representing ANSI escape codes for text color.
+    """
+    RESET = '\033[0m'
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    YELLOW = (255, 255, 0)
+    BLUE = (0, 0, 255)
+    MAGENTA = (255, 0, 255)
+    CYAN = (0, 255, 255)
+    WHITE = (255, 255, 255)
+    GRAY = (128, 128, 128)
+
+def fade_color(text, from_color_name, to_color_name, duration=1.0):
+    """
+    Fades the specified text from left to right with a gradual color change.
+    The user can specify the color names (e.g., "RED", "BLUE").
+    The number of steps is calculated based on the duration.
+    """
+    from_color = getattr(Colorr, from_color_name.upper(), Color.WHITE)
+    to_color = getattr(Colorr, to_color_name.upper(), Color.WHITE)
+
+    steps = int(duration * 10)
+    sleep_time = duration / steps
+
+    text_length = len(text)
+    r_step = (to_color[0] - from_color[0]) / text_length
+    g_step = (to_color[1] - from_color[1]) / text_length
+    b_step = (to_color[2] - from_color[2]) / text_length
+
+    for i, char in enumerate(text):
+        r = int(from_color[0] + i * r_step)
+        g = int(from_color[1] + i * g_step)
+        b = int(from_color[2] + i * b_step)
+
+        sys.stdout.write(f"\033[38;2;{r};{g};{b}m{char}")
+        sys.stdout.flush()
+        time.sleep(sleep_time)
+
+    sys.stdout.write(Color.RESET + "\n")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     # Example usage
     print(colorize_text("Hello, World!", Color.RED))
     print(fade_text("Hello, World!", FadedColor.YELLOW))
     print(rainbow_text("Hello, World!"))
+    fade_color("Hello, World!", "RED", "BLUE", duration=2.0)
